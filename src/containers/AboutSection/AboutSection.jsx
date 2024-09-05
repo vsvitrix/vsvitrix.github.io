a0d2c88aef76f56e5562e7ec269f3367a56f0948
BridgetteR <bridgetteriver@gmail.com>
2024-09-05 19:03:07 +1000

    Updated twitch player

---
+++ src/containers/AboutSection/AboutSection.jsx
@@ -0,0 +1,12 @@
+import styles from "./AboutSection.module.scss";
+
+const AboutSection = () => {
+	return (
+		<div>
+			<h1>About</h1>
+			<p>This is an introduction about a couple of goobers</p>
+		</div>
+	);
+};
+
+export default AboutSection;
